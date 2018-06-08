d1fdff4b05b598fcb38dfb9c02fd02a465dca326
Bär Halberkamp <fledaway@gmail.com>
2018-06-08 21:19:23 +0200

    Change theme of the day to writing prompt

--- plugins/daily.js
+++ plugins/daily.js
@@ -17,7 +17,7 @@ const ROOM = 'thelibrary';
 // All the specific configuration options for each of the daily (or weekly) commands.
 const dailies = {
 	totw: {
-		name: "Theme of the Week",
+		name: "Writing Prompt",
 		room: ROOM,
 		params: ['title', 'image', 'description'],
 		async renderEntry(entry, pm) {
